9e4aa78f84a8547be09b943799eeefa44f088825
Rohan-devv <devv112004@gmail.com>
2025-07-07 01:40:30 +0530

    deploy changes again

--- Server/src/db/connectTomongoDb.js
+++ Server/src/db/connectTomongoDb.js
@@ -1,12 +1,13 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const connectDB = async() => {
-    try {
-        const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}`)
-        console.log(`\n MongoDB connected , DB HOST: ${connectionInstance.connection.host}`);
-    } catch (error) {
-        console.log("MongoDb connection failed ", error)
-        process.exit(1);
-    }
-}
-export default connectDB
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`\n✅ MongoDB connected | HOST: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.error("❌ MongoDB connection failed:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
