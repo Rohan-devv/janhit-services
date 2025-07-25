6be91da8b0d8ce18ddfa6736fd1257ac634d7ac7
Rohan-devv <devv112004@gmail.com>
2025-07-26 05:16:51 +0530

    DOCKERIZE

--- Server/src/index.js
+++ Server/src/index.js
@@ -14,7 +14,7 @@ connectDB()
   .then(() => {
     console.log("MongoDB connected successfully"); // ✅ Add this
     const PORT = process.env.PORT || 8000;
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`Server is running on port ${PORT}`);
     });
   })
